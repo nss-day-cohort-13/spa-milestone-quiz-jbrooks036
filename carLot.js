05d5e051f020dbf2939f06f281f3f238a34d82b9
jbrooks036 <joannbrooks036@gmail.com>
2016-05-09 17:09:24 -0400

    read in JSON file into private inventory array of POJOs

--- carLot.js
+++ carLot.js
@@ -1,17 +1,29 @@
-var CarLot = (function () {
+// this is the first IIFE
+
+var CarLot = (function (CarLot) {
   var inventory = [];
 
-  return {
-    getInventory: function () {
-
+  // get the array of cars
+  CarLot.getInventory = function () {
+      return inventory;
     },
-    loadInventory: function (callback) {
-      var inventoryLoader = new XMLHttpRequest();
 
-      inventoryLoader.addEventListener("load", function () {
+  // load the cars from the JSON file into private array
+  CarLot.loadInventory = function (callback) {
+    var inventoryLoader = new XMLHttpRequest();
 
-      });
-    }
+    inventoryLoader.addEventListener("load", function () {
+      inventory = JSON.parse(this.responseText);
+      console.log("CarLot:17 / inventory = ", inventory);
+      callback(inventory);
+    });
+
+    inventoryLoader.open("GET", "inventory.json");
+    inventoryLoader.send();
   };
 
-})();
+  console.log("CarLot.js:25 / CarLot = ", CarLot);
+  return CarLot;
+
+})(CarLot || {});
+
