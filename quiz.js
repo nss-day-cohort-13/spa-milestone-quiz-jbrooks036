ac6a8d1276872d400aada8db5ca2d26c00748d62
jbrooks036 <joannbrooks036@gmail.com>
2016-05-13 15:20:16 -0400

    looping through carArray and beginning generation of HTML

--- quiz.js
+++ quiz.js
@@ -1,9 +1,21 @@
 var CarLot = (function (Carlot) {
 
   function populatePage (inventory) {
-    console.log("quiz.js:4 / inventory = ", inventory);
+    var carArray = inventory.cars;
+    var numCars = carArray.length;
 
     // Loop over the inventory and populate the page
+    for (i=0; i<numCars; i++) {
+      car = inventory.cars[i];
+      console.log("car = ", car);
+      carDetails = "";
+
+      // make html string for card
+      carDetails += `<h2>Make: ${car.make}</h2>`;
+
+      // post card to DOM
+      console.log("carDetails = ", carDetails);
+    }
 
     // Now that the DOM is loaded, establish all the event listeners needed
     // CarLot.activateEvents();
